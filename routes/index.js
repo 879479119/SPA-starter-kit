ac13fdbfb85c8e73d151979af9360b6c9653927e
RockSAMA <767444690@qq.com>
2016-10-19 20:06:22 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var sourcePort = require('../config').dev.sourcePort;
-if(process.env.npm_lifecycle_event === "start") sourcePort = 3000;
+if(process.env.npm_lifecycle_event === "start") sourcePort = 8888;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express',page: "video",sourcePort: sourcePort });
