4581fa818abcd8753411ecf4fd4882a1ff97506f
RockSAMA <767444690@qq.com>
2017-03-14 23:30:04 +0800

    Oh my broken heart

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,42 @@
-var webpack = require('webpack');
-var fs = require('fs');
-var globalConfig = require('./config');
-var exec = require('child_process').exec, child;
+let webpack = require('webpack');
+let fs = require('fs');
+let globalConfig = require('./config');
+// let exec = require('child_process').exec, child;
 
-var __DEV__ = process.env.npm_lifecycle_event === "webpack";
+let __DEV__ = process.env.npm_lifecycle_event === "webpack";
 
-var pages = JSON.parse(fs.readFileSync('routes/route.json', 'utf-8'));
-var entryObj = {};
+let pages = JSON.parse(fs.readFileSync('routes/route.json', 'utf-8'));
+let entryObj = {};
 
-pages.forEach(function(item,index){
+pages.forEach(function (item) {
 	entryObj[item.name] = __DEV__ ? [
-		item.src,
-		'webpack/hot/dev-server',
-		'webpack-dev-server/client?http://0.0.0.0:'+globalConfig.dev.sourcePort
-	] : item.src;
+			item.src,
+			'webpack/hot/dev-server',
+			'webpack-dev-server/client?http://0.0.0.0:' + globalConfig.dev.sourcePort
+		] : item.src;
 })
 
-var DevConfig = {
-	output : {
-		publicPath: "http://0.0.0.1:"+globalConfig.dev.sourcePort+"/output/js/",
-		path : __dirname + '/output/js',
-		filename : '[name].bundle.js'
+//noinspection JSUnresolvedFunction
+let DevConfig = {
+	output: {
+		// publicPath: "http://127.0.0.1:" + globalConfig.dev.sourcePort + "/output/js/",
+		path: __dirname + '/output/js',
+		filename: '[name].bundle.js',
+		publicPath: '/output/js/'
 	},
 	devServer: {
-      inline: true,
-      progress: true,
-			color: true,
-			//好像想要在手机上访问的话这里需要改成 0.0.0.0
-      host: '0.0.0.0',
-			port: globalConfig.dev.sourcePort,
-			contentBase: 'output/',
-			outputPublicPath: 'output/'
-  },
+		inline: true,
+		progress: true,
+		color: true,
+		host: '0.0.0.0',
+		port: globalConfig.dev.sourcePort,
+		// contentBase: 'output/',
+		publicPath: '/output/js/'
+	},
 	// 插件项
-	plugins : [
+	plugins: [
 		new webpack.DefinePlugin({
-    	'process.env.NODE_ENV': '"development"',
+			'process.env.NODE_ENV': '"development"',
 			'process.env.SOURCE_PORT': globalConfig.dev.sourcePort
 		}),
 		new webpack.HotModuleReplacementPlugin()
@@ -43,15 +44,13 @@ var DevConfig = {
 };
 
 //noinspection JSUnresolvedFunction
-var config = {
+let config = {
 	// 页面入口文件配置
-	entry : (function(){
-		return entryObj
-	})(),
+	entry: entryObj,
 	// 入口文件输出配置
-	output : {
-		path : __dirname + '/output/js/',
-		filename : '[name].bundle.js',
+	output: {
+		path: __dirname + '/output/js/',
+		filename: '[name].bundle.js',
 	},
 	externals: {
 		"jquery": "jQuery",
@@ -61,23 +60,26 @@ var config = {
 	module: {
 		// 加载器配置
 		loaders: [
-			{	test: /\.(jsx|js)$/,
+			{
+				test: /\.(jsx|js)$/,
 				exclude: /node_modules/,
 				loader: 'babel',
-				//很奇怪，不需要进行query配置或者是.babelrc文件，用了会报错
 				query: {
-					presets: ["react",'es2015','stage-0']
+					presets: ["react", 'es2015', 'stage-0']
 				}
 			},
-			{	test: /\.css$/,
+			{
+				test: /\.css$/,
 				exclude: /node_modules/,
 				loader: 'style!css'
 			},
-			{ test: /\.less$/,
+			{
+				test: /\.less$/,
 				loader: 'style!css!less'
 			},
-			{ test: /\.(png|jpg|gif)$/,
-      	        loader: 'url?limit=5000000'
+			{
+				test: /\.(png|jpg|gif)$/,
+				loader: 'url?limit=5000'
 			}
 		]
 	},
@@ -85,12 +87,7 @@ var config = {
 	resolve: {
 		extensions: ['', '.js', '.jsx', '.css', '.json', 'less']
 	},
-
-	externals: {
-		jquery: "jquery"
-	},
-
-	plugins : [
+	plugins: [
 		new webpack.DefinePlugin({
 			'process.env.SOURCE_PORT': globalConfig.dev.sourcePort
 		})
@@ -98,9 +95,9 @@ var config = {
 };
 
 //进入开发者模式
-if(__DEV__){
-	var prop;
-	for(prop in DevConfig){
+if (__DEV__) {
+	let prop;
+	for (prop in DevConfig) {
 		config[prop] = DevConfig[prop]
 	}
 	//启动后台nodejs服务器
